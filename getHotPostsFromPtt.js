f1d3741f6bb57a0a6127e0de4b1329085ba15695
evanhongo <evanhongo@gmail.com>
2021-02-14 19:01:07 +0800

    refactor: getting data from different source are splitted into three parts

--- getHotPostsFromPtt.js
+++ getHotPostsFromPtt.js
@@ -2,17 +2,19 @@ import axios from "axios";
 import cheerio from "cheerio";
 
 const getHotPostsFromPtt = async () => {
-  const res = await axios.get(process.env.PTT_URL).catch((err) => {
+  try {
+    const res = await axios.get(process.env.PTT_URL);
+    const { data: htmlContent } = res;
+    const postIds = [];
+    const $ = cheerio.load(htmlContent);
+    $("a.e7-article-default").each((i, elem) => {
+      const title = $(elem).children().first().children().first().text();
+      postIds[i] = `${title}\nhttps://www.pttweb.cc${$(elem).attr("href")}`;
+    });
+    return postIds.slice(0, 20).join("\n");
+  } catch (err) {
     throw Error(err);
-  });
-  const { data: htmlContent } = res;
-  const postIds = [];
-  const $ = cheerio.load(htmlContent);
-  $("a.e7-article-default").each((i, elem) => {
-    const title = $(elem).children().first().children().first().text();
-    postIds[i] = `${title}\nhttps://www.pttweb.cc${$(elem).attr("href")}`;
-  });
-  return postIds.slice(0, 20).join("\n");
+  }
 };
 
 export default getHotPostsFromPtt;
--- getPopularSongs.js
+++ getPopularSongs.js
@@ -17,82 +17,118 @@ const musicCategoryMap = {
 };
 
 const cache = new NodeCache({ stdTTL: 60 * 60 * 6 });
+let db, collection;
+class Node {
+  constructor(fn) {
+    this.task = fn;
+    this.next = null;
+  }
 
-const getPopularSongs = async (category) => {
-  //data flow: cache => mongodb => third party api
-  let msg;
-  if (cache.has(category)) return cache.get(category);
-  else {
-    const mongoClient = mongodb.MongoClient;
-    const db = await mongoClient
-      .connect(process.env.DB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      })
-      .catch((err) => {
-        db.close();
-        throw Error(err);
-      });
-    const dbo = db.db("meow-db");
-    const collection = dbo.collection("song");
-    await collection
-      .createIndex({ updatedAt: 1 }, { expireAfterSeconds: 60 * 60 * 12 })
-      .catch((err) => {
-        console.error(err);
-      });
-    const songs = await collection.findOne({ category }).catch((err) => {
-      db.close();
-      throw Error(err);
-    });
-    msg = songs?.info;
-    if (!msg) {
-      console.info("Not found in mongo");
-      const res = await axios
-        .get(process.env.KKBOX_URL, {
-          params: {
-            category: musicCategoryMap[category],
-            lang: "tc",
-            limit: 3,
-            terr: "tw",
-            type: "song",
-          },
-        })
-        .catch((err) => {
-          throw Error(err);
-        });
+  setNext(node) {
+    this.next = node;
+    return node;
+  }
 
-      const {
-        data: {
-          data: {
-            charts: { song: songs },
-          },
-        },
-      } = res;
+  getNext() {
+    return this.next;
+  }
 
-      const songInfos = songs.map((song) => ({
-        name: song.song_name,
-        artist: song.artist_name,
-      }));
-
-      const vedioIds = await getVedioIds(songInfos).catch((err) => {
-        throw Error(err);
-      });
-
-      msg = songs
-        .map(
-          (song, i) =>
-            `${song.song_name}\n${song.artist_name}\nhttps://www.youtube.com/watch?v=${vedioIds[i]}`
-        )
-        .join("\n");
-
-      collection.updateOne(
-        { category },
-        { $set: { category, info: msg, updatedAt: new Date() } },
-        { upsert: true }
-      );
+  async handleRequest(req) {
+    let tmpNode = this;
+    let res;
+    while (tmpNode && (res = await tmpNode.task(req)) === "hasNext") {
+      tmpNode = tmpNode.getNext();
     }
+    return res;
+  }
+}
+
+const getSongsFromCache = async (category) => {
+  if (cache.has(category)) return cache.get(category);
+  else return "hasNext";
+};
+
+const getSongsFromMongo = async (category) => {
+  try {
+    console.info("Not found in cache");
+    const mongoClient = mongodb.MongoClient;
+    db = await mongoClient.connect(process.env.DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    const dbo = db.db("meow-db");
+    collection = dbo.collection("song");
+    await collection.createIndex(
+      { updatedAt: 1 },
+      { expireAfterSeconds: 60 * 60 * 12 }
+    );
+    const songs = await collection.findOne({ category });
+    const msg = songs?.info;
+    if (msg) {
+      db.close();
+      cache.set(category, msg);
+      return msg;
+    } else return "hasNext";
+  } catch (err) {
+    throw Error(err);
+  }
+};
+
+const getSongsFromThirdPartyApi = async (category) => {
+  try {
+    console.info("Not found in mongo");
+    const res = await axios.get(process.env.KKBOX_URL, {
+      params: {
+        category: musicCategoryMap[category],
+        lang: "tc",
+        limit: 10,
+        terr: "tw",
+        type: "song",
+      },
+    });
+    const {
+      data: {
+        data: {
+          charts: { song: songs },
+        },
+      },
+    } = res;
+    const songInfos = songs.map((song) => ({
+      name: song.song_name,
+      artist: song.artist_name,
+    }));
+    const vedioIds = await getVedioIds(songInfos);
+    const msg = songs
+      .map(
+        (song, i) =>
+          `${song.song_name}\n${song.artist_name}\nhttps://www.youtube.com/watch?v=${vedioIds[i]}`
+      )
+      .join("\n");
+    await collection.updateOne(
+      { category },
+      { $set: { category, info: msg, updatedAt: new Date() } },
+      { upsert: true }
+    );
+    db.close();
     cache.set(category, msg);
     return msg;
+  } catch (err) {
+    throw Error(err);
+  }
+};
+
+const getPopularSongs = async (category) => {
+  //data source: cache => mongodb => third party api
+  const node1 = new Node(getSongsFromCache);
+  const node2 = new Node(getSongsFromMongo);
+  const node3 = new Node(getSongsFromThirdPartyApi);
+  node1.setNext(node2).setNext(node3);
+  try {
+    const songs = await node1.handleRequest(category);
+    return songs;
+  } catch (err) {
+    throw Error(err);
   }
 };
 
