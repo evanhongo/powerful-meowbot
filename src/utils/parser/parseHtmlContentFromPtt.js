465b19442b421d8711eae2c4960ce57bb0d4b588
evanhongo <evanhongo@gmail.com>
2021-08-06 18:13:36 +0800

    fix(parseHtmlContentFromPtt): remove duplicated article

--- src/utils/parser/parseHtmlContentFromPtt.js
+++ src/utils/parser/parseHtmlContentFromPtt.js
@@ -2,13 +2,14 @@ import cheerio from "cheerio";
 
 const parseHtmlContentFromPtt = (htmlContent) => {
   const $ = cheerio.load(htmlContent);
-  const posts =
+  let posts =
     $("a.e7-article-default")
       .map((i, elem) => {
         const title = $(elem).children().first().children().first().text();
         return `${title}\nhttps://www.pttweb.cc${$(elem).attr("href")}`;
       })
       .get();
+  posts = posts.filter(post => post.indexOf("\n") != 0)
   return posts;
 };
 
